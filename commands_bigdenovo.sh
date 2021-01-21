978198e9aa71af6d5e41d91059753027f82b3812
Noort, Maite van den <m.vandennoort@nioo.knaw.nl>
2021-01-21 15:59:10 +0100

    Add commands_bigdenovo.sh

---
+++ commands_bigdenovo.sh
@@ -0,0 +1,11 @@
+#merging the Watson_R1.fq.gz file from de demultiplexed reads from library 1 and library 2. This results in one Watson_R1.fq.gz file in the output #directory output_demultiplex from the big de novo. 
+Cat "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_demultiplex/Watson_R1.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane8/output_demultiplex/Watson_R1.fq.gz" > "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/big_de_novo/output_big_denovo/output_demultiplex/Watson_R1.fq.gz"	
+
+#merging the Watson_R2.fq.gz file from de demultiplexed reads from library 1 and library 2. This results in one Watson_R2.fq.gz file in the output #directory output_demultiplex from the big de novo.
+Cat "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_demultiplex/Watson_R2.fq.gz " "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane8/output_demultiplex/Watson_R2.fq.gz " > "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/big_de_novo/output_big_denovo/output_demultiplex/Watson_R2.fq.gz "
+
+#merging the Crick_R1.fq.gz file from de demultiplexed reads from library 1 and library 2. This results in one Crick_R1.fq.gz file in the output #directory output_demultiplex from the big de novo.
+Cat "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_demultiplex/Crick_R1.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane8/output_demultiplex/Crick_R1.fq.gz" > "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/big_de_novo/output_big_denovo/output_demultiplex/Crick_R1.fq.gz"	
+
+#merging the Crick_R2.fq.gz file from de demultiplexed reads from library 1 and library 2. This results in one Crick_R2.fq.gz file in the output #directory output_demultiplex from the big de novo.
+Cat "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_demultiplex/Crick_R2.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane8/output_demultiplex/Crick_R2.fq.gz" > "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/big_de_novo/output_big_denovo/output_demultiplex/Crick_R2.fq.gz"	
