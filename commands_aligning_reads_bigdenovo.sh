08995151c638786df07d08ab1e4f0f8cfc98f092
Noort, Maite van den <m.vandennoort@nioo.knaw.nl>
2021-01-21 16:30:09 +0100

    Update commands_aligning_reads_bigdenovo.sh

--- commands_aligning_reads_bigdenovo.sh
+++ commands_aligning_reads_bigdenovo.sh
@@ -8,18 +8,18 @@ Ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/big_de_no
 #This ensures that there is a link to the created big de novo reference file to lib2
 Ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/big_de_novo/output_big_denovo/output_denovo/consensus_cluster.renamed.fa" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/library2/outputlib2/output_denovo/"
 
-#This ensures that there is a link to the unassembled R1 reads from library 1 
+#This ensures that there is a link to the unassembled R1 reads from library 1
 Ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_denovo/Unassembled.R1.watson.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/library1/output_lib1/output_denovo/"	
-#This ensures that there is a link to the unassembled R2 reads from library 1 
+#This ensures that there is a link to the unassembled R2 reads from library 1
 Ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_denovo/Unassembled.R2.crick.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/library1/output_lib1/output_denovo/"	
-#This ensures that there is a link to the assembled reads from library 1 
+#This ensures that there is a link to the assembled reads from library 1
 Ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_denovo/Assembled.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/library1/output_lib1/output_denovo/"	
 
-#This ensures that there is a link to the unassembled R1 reads from library 2 
+#This ensures that there is a link to the unassembled R1 reads from library 2
 Ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_denovo/Unassembled.R1.watson.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/library2/outputlib2/output_denovo/"
-#This ensures that there is a link to the unassembled R2 reads from library 2 
+#This ensures that there is a link to the unassembled R2 reads from library 2
 Ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_denovo/Unassembled.R2.crick.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/library2/outputlib2/output_denovo/"
-#This ensures that there is a link to the assembled reads from library 2 
+#This ensures that there is a link to the assembled reads from library 2
 Ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_denovo/Assembled.fq.gz" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/big_denono/library2/outputlib2/output_denovo/"
 
 
