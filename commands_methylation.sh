4e42d4c66409e9f342fbe8e81ded36715e07f983
Noort, Maite van den <m.vandennoort@nioo.knaw.nl>
2021-01-21 16:28:05 +0100

    Update commands_methylation.sh

--- commands_methylation.sh
+++ commands_methylation.sh
@@ -3,9 +3,9 @@ ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_
 #This ensures that there is a link to the output_demulitplex directory of the original library 2 output
 ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane8/output_demultiplex/" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/lane8_thenovo2/epiGBS2/Output_lane8_thenovo2/"	
 
-#This ensures that there is a link to the file with de novo of library 1  
+#This ensures that there is a link to the file with de novo of library 1
 ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane2/output_denovo/consensus_cluster.renamed.fa" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/lane2_denovo8/epigbs2/output/output_denovo/"	
-#This ensures that there is a link to the file with de novo of library 2  
+#This ensures that there is a link to the file with de novo of library 2
 ln -s "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/output_lane8/output_denovo/consensus_cluster.renamed.fa" "/mnt/nfs/bioinfdata/home/NIOO/maiten/duckweed_epiGBS/lane8_thenovo2/epiGBS2/Output_lane8_thenovo2/output_denovo/"	
 
 #This ensures that there is a link to the unassembled R1 reads from library 1
